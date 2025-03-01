4e2c9d18ff4dc7674ebccfc13cff3fd26f0e135b
nikitenkovit <nelot@mail.ru>
2025-03-01 20:51:38 +0300

    вынесет все типы в отдельную папку

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,27 +1,6 @@
-console.log('Фоновый скрипт загружен.');
+import { Rule } from '../types';
 
-/*
- * Интерфейс для описания правил перехвата запросов.
- * Каждое правило содержит:
- * - id: Уникальный идентификатор правила.
- * - path: Часть URL, которую нужно перехватывать.
- * - data: Mock-данные, которые будут возвращены вместо реального ответа (необязательное поле).
- * - isActive: Флаг, указывающий, активно ли правило.
- * - delay: Задержка в миллисекундах перед возвратом mock-ответа (необязательное поле).
- * - responseType: Тип ответа (success, error, redirect).
- * - errorMessage: Текст ошибки.
- * - redirectUrl: URL для редиректа.
- */
-interface Rule {
-	id: string;
-	path: string;
-	data?: string;
-	isActive: boolean;
-	delay?: number;
-	responseType: 'success' | 'error' | 'redirect';
-	errorMessage?: string;
-	redirectUrl?: string;
-}
+console.log('Фоновый скрипт загружен.');
 
 // Глобальный массив для хранения правил перехвата запросов.
 let rules: Rule[] = [];
---
+++ src/types/index.ts
@@ -0,0 +1,47 @@
+/*
+ * Интерфейс для описания правил перехвата запросов.
+ * Каждое правило содержит:
+ * - id: Уникальный идентификатор правила.
+ * - path: Часть URL, которую нужно перехватывать.
+ * - data: Mock-данные, которые будут возвращены вместо реального ответа (необязательное поле).
+ * - isActive: Флаг, указывающий, активно ли правило.
+ * - delay: Задержка в миллисекундах перед возвратом mock-ответа (необязательное поле).
+ * - responseType: Тип ответа (success, error, redirect).
+ * - errorMessage: Текст ошибки.
+ * - redirectUrl: URL для редиректа.
+ */
+export interface Rule {
+	id: string;
+	path: string;
+	data?: string;
+	isActive: boolean;
+	delay?: number;
+	responseType: 'success' | 'error' | 'redirect';
+	errorMessage?: string;
+	redirectUrl?: string;
+}
+
+/*
+ * Интерфейс для пропсов компонента Rule.
+ */
+export interface RuleProps {
+	rule: Rule;
+	isExtensionActive: boolean;
+	updateRule: (
+		id: string,
+		field: keyof Rule,
+		value: string | boolean | number
+	) => void;
+	clearRuleFields: (id: string) => void;
+	deleteRule: (id: string) => void;
+	rulesCount: number;
+}
+
+/*
+ * Интерфейс для сообщений, отправляемых между popup и background.
+ */
+export interface ExtensionMessage {
+	action: 'updateRules' | 'activateExtension' | 'deactivateExtension' | 'error';
+	rules?: Rule[];
+	error?: string;
+}
